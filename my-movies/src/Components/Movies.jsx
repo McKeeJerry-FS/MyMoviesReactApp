638034caab6134e61591162b0ce2e0bea263f947
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-08-02 11:28:53 -0400

    creating a react project

---
+++ my-movies/src/Components/Movies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Movie = () => {
+    return (
+        <div>
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+        </div>
+    )
+}
